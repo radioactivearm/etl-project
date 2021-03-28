6b2c139019d6d40484b398e3e93c4dbb41450b5d
kunaual <aekunau@gmail.com>
2021-03-28 14:35:38 -0500

    updating comments for latitude conv. issues + adding file urls

--- query.sql
+++ query.sql
@@ -1,17 +1,20 @@
+/* joining region with flavor. The lower isn't actually necessary. */
 select r.distillery, r.region, r.district,
        f.body, f.sweetness, f.smoky, f.medicinal, f.tobacco, f.honey, f.spicy, f.winey,
        f.nutty, f.malty, f.fruity, f.floral
 from scotch_region r, scotch_flavor f
 where lower(f.distillery) = lower(r.distillery);
 
+-- joining region w/location
 select r.*, l.postcode
 from scotch_region r, scotch_location l
 where l.distillery = r.distillery;
 
+/*checking out the regions that aren't in the flavor table*/
 select * from scotch_region
 where distillery not in (select distillery from scotch_flavor);
 
-
+/* checking each table, just to double check no weird characters in distillery name was causing this one to not be found on each table */
 select * from scotch_region where lower(distillery) like '%lagav%';
 select * from scotch_flavor where lower(distillery) like '%lagav%';
 select * from scotch_location where lower(distillery) like '%lagav%';
